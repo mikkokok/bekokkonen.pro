77d9c923cbb3542abdbaaacc0ae4d0d498efa0e1
Mikko Kokkonen <mikko.kokkonen@outlook.com>
2026-08-25 19:54:21 +0300

    Some updates left

--- Models/HeatHarmony/EmLatestResponse.cs
+++ Models/HeatHarmony/EmLatestResponse.cs
@@ -4,6 +4,7 @@
     public sealed class EmLatestResponse
     {
         public DateTime lastEnabled { get; set; }
+        public DateTime lastDisabled { get; set; }
         public bool isOverridden { get; set; }
         public bool isRunning { get; set; }
         public bool isOn { get; set; }
